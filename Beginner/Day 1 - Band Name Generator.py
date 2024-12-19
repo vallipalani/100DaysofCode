280e83b3ad6e8f0e723ab01f0814606cd9fca506
Valliammai Palaniappan <valligems@gmail.com>
2024-12-19 23:37:21 +0400

    Upload Day 1

---
+++ Beginner/Day 1 - Band Name Generator.py
@@ -0,0 +1,4 @@
+print("Hello User! Welcome to the Band Name Generator!")
+city = input("What city did you grow up in?\n")
+pet = input("What is your pet's name?\n")
+print("Your band name could be " + city + " " + pet)
